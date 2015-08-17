6e5014ac845528d1a0c6f19468a6e9f1507da545
Liang Tang <liang.xjtu@gmail.com>
2015-08-17 16:37:48 -0500

    exer 204

--- src/main/scala/chapter2/main.scala
+++ src/main/scala/chapter2/main.scala
@@ -1,4 +1,4 @@
-
+package chapter2;
 
 object Excerses201 extends App{
 
@@ -55,16 +55,14 @@ object Excerses203 extends App{
   MyThread.thread{
     for(i <- 0 until 15){
       elem.synchronized{
-        println("before producer")
-        elem.wait()
+        while(elem.isEmpty == false)elem.wait()
         elem.put(i)
-        println("during producer")
         elem.notify()
-        println("after producer")
       }
     }
   }
   MyThread.thread{
+
     while(true){
       elem.synchronized{
         println("before wait")
@@ -77,6 +75,65 @@ object Excerses203 extends App{
     }
   }
 }
+
+object Excerses204Answer extends App{
+
+  // 4
+  class SyncVar2[T] {
+    private var state: Option[T] = None
+    def get(): T = state.synchronized {
+      state match {
+        case None => throw new IllegalArgumentException()
+        case Some(x) =>
+          val tmp: T = x
+          state = None // if state.notify() was called afterwards, an IllegalMonitorStateExc would be thrown as the current thread does not own the object monitor anymore; it was lost on assigning None to state
+          tmp
+      }
+    }
+    def put(el: T): Unit = state.synchronized {
+      state match {
+        case None => state = Some(el)
+        case Some(x) => throw new IllegalArgumentException()
+      }
+    }
+    def isEmpty: Boolean = state.synchronized {
+      state match {
+        case None => true
+        case _ => false
+      }
+    }
+    def nonEmpty: Boolean = !isEmpty
+  }
+
+   val sv2 = new SyncVar2[Int]
+   val tprod = MyThread.thread {
+      sv2.synchronized{
+        for(i <- 0 until 15){
+        println("before producer")
+        while(sv2.nonEmpty)sv2.wait()
+        sv2.put(i)
+        println("during producer")
+        sv2.notify()
+        println("after producer")
+      }
+    }
+   }
+   val tcons = MyThread.thread {
+
+     def go(): Unit = {
+       var get: Int = 0
+       sv2.synchronized {
+         while (sv2.isEmpty) sv2.wait()
+         get = sv2.get()
+         sv2.notify()
+       }
+       println(get)
+       if (get < 14) go()
+     }
+     go()
+   }
+}
+
 
 object MyThread {
   def thread(body: => Unit):java.lang.Thread = {
